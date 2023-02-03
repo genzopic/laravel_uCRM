8558a55b3aa5f0882b6fe6df43b515241db573d0
genzo <genzo.pic@gmail.com>
2023-02-03 15:00:33 +0900

    商品一覧準備

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -16,6 +17,7 @@ class ItemController extends Controller
     public function index()
     {
         //
+        return Inertia::render('items/index');
     }
 
     /**
